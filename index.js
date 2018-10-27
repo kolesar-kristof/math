e0f650793ea6ded61b1a8239e05bb07f4f16d094
Kolesár András <kolesar@kolesar.hu>
2018-10-27 14:38:34 +0200

    count lives

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
+var lives = 5;
+var success = false;
+
 var left = Math.floor(Math.random() * 100);
 var right = Math.floor(Math.random() * 100);
 
 const readline = require('readline');
 const interface = readline.createInterface(process.stdin, process.stdout);
 
-interface.setPrompt(left + '+' + right + '=');
+interface.setPrompt(lives + ' életed van. ' + left + '+' + right + '=');
 interface.prompt();
+
 interface.on('line', function(line) {
     if (line == left+right) {
         console.log('Remek!');
+        success = true;
         interface.close();
     }
-    interface.prompt();
-}).on('close',function(){
+    lives -= 1;
+    if (lives > 0) {
+        interface.setPrompt(lives + ' életed van. ' + left + '+' + right + '=');
+        interface.prompt();
+    } else {
+        console.log('Meghaltál. Az eredmény ' + left + '+' + right + '=' + (left+right) + ' lett volna.');
+        interface.close();
+    }
+}).on('close',function() {
+    if (lives > 0 && !success) {
+        console.log();
+        console.log('Feladtad?');
+    }
     process.exit(0);
 });
