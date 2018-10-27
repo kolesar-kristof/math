982ee95979e46d657219d6c610e1bbf96c405eeb
Kolesár András <kolesar@kolesar.hu>
2018-10-27 13:53:17 +0200

    wait for console input and validate

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 var left = Math.floor(Math.random() * 100);
 var right = Math.floor(Math.random() * 100);
 
-console.log(left + '+' + right);
+const readline = require('readline');
+const interface = readline.createInterface(process.stdin, process.stdout);
+
+interface.setPrompt(left + '+' + right + '=');
+interface.prompt();
+interface.on('line', function(line) {
+    if (line == left+right) {
+        console.log('Remek!');
+        interface.close();
+    }
+    interface.prompt();
+}).on('close',function(){
+    process.exit(0);
+});
